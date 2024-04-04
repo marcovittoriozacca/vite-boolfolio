d7964aa4420df916b1f53a7f565428d0d98974cb
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-04 12:49:27 +0200

    feat: Projects from API in console

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script>
+import axios from 'axios';
+export default{
+  
+  methods:{
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+        console.log(res.data.projects);
+      })
+
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+
+
+}
 
 </script>
 
 <template>
   
+<h1 class="text-center">Ciao mondo</h1>
+
 </template>
 
 <style lang="scss" scoped>
